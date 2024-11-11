d9d3d955dce2909adfa0d34b0c6547277b602b66
girl-on-a-string <frenchfries55@protonmail.com>
2024-11-11 09:41:06 -0600

    created winning combos array

--- index.js
+++ index.js
@@ -15,14 +15,32 @@
 
 function player (name, marker) {
     return {
-
+        name, marker
     }
 }
 
 // game logic
 
 function gameLogic () {
-    
+    let gameboardObject = gameboard();
+    let isGameOver = false;
+
+    const winningCombos = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9],
+        [1, 5, 9],
+        [3, 5, 7],
+        [1, 4, 7],
+        [2, 5, 8],
+        [3, 6, 9]
+    ]
+
+    if (isGameOver) { //false
+
+    } else { //true
+
+    }
 }
 
 // put it all together
