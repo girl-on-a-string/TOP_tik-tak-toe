2bd43f39a1a1ffcd3642894e835f96851789d20e
girl-on-a-string <frenchfries55@protonmail.com>
2024-11-18 18:35:30 -0600

    created basic setup

--- index.js
+++ index.js
@@ -3,16 +3,8 @@ const gameboard = (() => { //iife
 
     //place markers
 
-    const placeMarker = (marker, index) => {
-        marker = player.marker;
-
-        if (!gameboardArray[index]) {
-            gameboardArray[index] = marker;
-            console.log(marker);
-            return true
-        } else {
-            return false
-        }
+    const placeMarker = () => {
+        
     }
 
     //
@@ -30,10 +22,6 @@ const gameboard = (() => { //iife
 function player (name, marker) {
     let score = 0;
 
-    const increaseScore = () => {
-        score++;
-    }
-
     return {
         name, marker, score
     }
@@ -44,75 +32,10 @@ function player (name, marker) {
 const gameLogic = (() => { //iife
     let isGameOver = false;
 
-    //change player 
-
-    const changePlayer = () => {
-        currentPlayer = currentPlayer === currentPlayer ? playerOne : playerTwo;
-        console.log(currentPlayer);
-    }
-
-    // finding winner
-
-    const winningCombos = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 4, 8],
-        [2, 4, 6],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8]
-    ]
-
-    const findWinner = (currentPlayer) => {
-        for (let i = 0; i < winningCombos.length(); i++) {
-            const cellOne = winningCombos[0];
-            const cellTwo = winningCombos[1];
-            const cellThree = winningCombos[2];
-
-            if (cellOne == "" || cellTwo == "" || cellThree == "") {
-                continue;
-            }
-
-            if (cellOne == cellTwo && cellTwo == cellThree) { //all spots filled with same marker
-                isGameOver = true;
-                break;
-            }
-
-            if (isGameOver == true) {
-                const resultDisplay = document.getElementById("result");
-                resultDisplay.innerText = `${currentPlayer} wins!`;
-            }
-        }
-    }
-
-    //reset game 
-
-    function resetGame () {
-        gameboardArray.fill(null);
-        player.score = 0;
-    }
-
-    //update turn status
-
-    // const updateTurnStatus = () => {
-    //     document.getElementById("player-num").innerText = currentPlayer ? playerOne : playerTwo;
-    //     console.log(currentPlayer);
-    // }
-
-    // play game
-
-    const playGame = () => {
-        if (isGameOver == false) { //false
-            changePlayer;
-
-        } else { //true
     
-        }
-    }
 
     return {
-        playGame, findWinner, resetGame, updateTurnStatus
+        
     }
 });
 
@@ -128,18 +51,11 @@ const cell = document.querySelectorAll(".cell");
 cell.forEach(cell => {
     cell.addEventListener("click", () => {
         console.log("clicked");
-        gameLogic.playGame;
-        gameLogic.updateTurnStatus;
-        
-        if (cell.innerText == "") {
-            gameboard.placeMarker;
-        }
     });
 });
 
 const resetBtn = document.getElementById("restart");
 resetBtn.addEventListener("click", () => {
-    gameLogic.resetGame;
     console.log("resetting");
 });
 
