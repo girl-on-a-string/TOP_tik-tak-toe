f20d138e4ec7d9ad652b9bbb4eca35d5bfb8f5c1
girl-on-a-string <frenchfries55@protonmail.com>
2024-11-19 19:48:50 -0600

    specified start button type and removed prevent default

--- index.js
+++ index.js
@@ -14,24 +14,10 @@ const gameboard = (() => { //iife
         console.log("rendering board");
     }
 
-    //place markers
-
-    const placeMarker = (index) => {
-        if (gameboardArray[index] == "") { // if array item is empty
-            gameboardArray[index] = player.marker; // puts marker on cell space WITHIN ARRAY
-            turn++
-            return true
-        } else {
-            return false
-        }
-    }
-
-    //
-
     console.log("gameboard working");
 
     return {
-        placeMarker, getGameboard, renderBoard
+        renderBoard
     }
 })();
 
@@ -39,10 +25,8 @@ const gameboard = (() => { //iife
 // players
 
 function player (name, marker) {
-    let score = 0;
-
     return {
-        name, marker, score
+        name, marker
     }
 }
 
@@ -53,41 +37,6 @@ const gameLogic = (() => { //iife
 
     let isGameOver = false;
     let turn = 0; 
-
-    // change player 
-
-    const changePlayer = () => {
-
-    }
-
-    // finding winner
-
-    const winningCombos = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 4, 8],
-        [2, 4, 6],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8]
-    ]
-
-    const checkWinner = () => {
-
-    }
-
-    // increase score
-
-    const increaseScore = () => {
-
-    }
-
-    // reset game
-
-    const resetGame = () => {
-        
-    }
 
     // start game 
 
@@ -104,23 +53,18 @@ const gameLogic = (() => { //iife
 
 const startBtn = document.getElementById("start-game");
 
-(() => {
-    console.log("program running");
-
-    startBtn.addEventListener("click", (e) => {
-        e.preventDefault();
+startBtn.addEventListener("click", () => {
     
-        const modal = document.getElementById("modal");
-        modal.style.display = "none";
+    // const modal = document.getElementById("modal");
+    // modal.style.display = "none";
     
-        const playerOne = player(document.querySelector("#playerOne-input").value, "X");
-        const playerTwo = player(document.querySelector("#playerTwo-input").value, "O");
+    // const playerOne = player(document.querySelector("#playerOne-input").value, "X");
+    // const playerTwo = player(document.querySelector("#playerTwo-input").value, "O");
     
-        console.log("game initiated");
+    console.log("game initiated");
     
-        console.log(gameLogic.start);
-    });
-})();
+    gameLogic.start
+});
 
 
 
